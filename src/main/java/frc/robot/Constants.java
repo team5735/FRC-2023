b5ce81a3372656490d922025f686ff86d60a598e
Carman25 <59984273+Carman25@users.noreply.github.com>
2023-03-19 11:39:40 -0400

    Some More Path Updates
    Holonomic rotation to make 2 cue work.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -244,8 +244,8 @@ public final class Constants {
         public static class ExtenderConstants {
                 public static final double EXTENDER_MAX_LENGTH = Units.inchesToMeters(46.0); // 1.1684 meters
 
-                public static final double EXTEND_MOTOR_OUT_SPEED = -.1;
-                public static final double EXTEND_MOTOR_IN_SPEED = .1;
+                // public static final double EXTEND_MOTOR_OUT_SPEED = -.1;
+                // public static final double EXTEND_MOTOR_IN_SPEED = .1;
                 
                 public static final int EXTEND_MOTOR_ID = 10;
                 
